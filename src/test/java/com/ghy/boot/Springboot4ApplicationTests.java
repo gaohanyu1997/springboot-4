6d7487d77654820f31de1e2a3075537a1c80fb48
gaohanyu <2405662372@qq.com>
2022-06-20 21:36:58 +0800

    Initial commit

---
+++ src/test/java/com/ghy/boot/Springboot4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghy.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
